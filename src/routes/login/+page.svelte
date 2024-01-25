db60a2353aeffc0e7e4eb07abb54d9073545cc87
vladpavlovhz <pavl0008@hz.nl>
2024-01-25 10:39:01 +0100

    removed non-func button

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -139,13 +139,5 @@
         Don't have an account? Register
         </button>
       </div>
-      <div>
-        <button 
-        type="button"
-          class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          on:click={googleLogin}>
-          Login with Google
-        </button>
-      </div>
   </div>
 </main>
